d4688737d9a744994154b99018fc88bc9e525e80
rozetadavchevska <rozetadavcevska@gmail.com>
2022-05-18 03:32:32 +0200

    Compressed assets

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-// import logo from 'assets/logo.png';
+import React  from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
+import {Route, Routes} from 'react-router-dom';
 import {Header} from './components/components';
-//import {Home} from './pages/pages';
-
+import {Home} from './pages/pages';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={ <Home /> }/>
+      </Routes>
     </div>
   );
 }
