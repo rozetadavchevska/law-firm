39ae6ebb63b4dd39c652c5c432c531e3d69b57d0
rozetadavchevska <rozetadavcevska@gmail.com>
2022-09-09 00:15:44 +0200

    Update

--- php/formValidation.php
+++ php/formValidation.php
@@ -1,19 +1,28 @@
 <?php
+    $rest_json = file_get_contents("php://input");
+    $_POST = json_decode($rest_json, true);
+    
+    if (empty($_POST['fname']) && empty($_POST['email'])) die();
+
+
 
     if (isset($_POST['submit'])){
         $firstName = $_POST['fname'];
         $lastName = $_POST['lname'];
         $emailFrom = $_POST['email'];
         $message = $_POST['message'];
+
+
+        $emailTo = "rozetadavcevska@gmail.com";
+        $headers = "From: " .$firstName .$lastName. 
+        "\r\n Email: ".$emailFrom. 
+        "\r\n Message: " .$message. "\r\n";
+
+        mail($emailTo, $firstName, $message, $headers);
+    } else{
+        echo json_encode(["sent" => false, "message" => "Something went wrong"]);
     }
 
-    $emailTo = "rozetadavcevska@gmail.com";
-    $headers = "From: " .$firstName .$lastName. 
-    "\r\n Email: ".$emailFrom. 
-    "\r\n Message: " .$message. "\r\n";
-
-    if(mail($emailTo,$firstName, $message, $headers)){
-        $success="Your message is received successfully";
-    };
+   
 
 ?>
