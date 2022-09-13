0946f6202aef83ffcf7d0df0fd85c1fa76b63b49
rozetadavchevska <rozetadavcevska@gmail.com>
2022-09-13 08:46:39 +0200

    Update

--- php/formValidation.php
+++ php/formValidation.php
@@ -1,28 +1,70 @@
 <?php
-    $rest_json = file_get_contents("php://input");
-    $_POST = json_decode($rest_json, true);
-    
-    if (empty($_POST['fname']) && empty($_POST['email'])) die();
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
 
+$rest_json = file_get_contents("php://input");
+$_POST = json_decode($rest_json, true);
 
+$errors = array();
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    if (empty($_POST['email'])) {
+        $errors[] = 'Email is empty';
+    } else {
+        $email = $_POST['email'];
+        $fName = $_POST['fname'];
+        $lName = $_POST['lname'];
 
-    if (isset($_POST['submit'])){
-        $firstName = $_POST['fname'];
-        $lastName = $_POST['lname'];
-        $emailFrom = $_POST['email'];
-        $message = $_POST['message'];
-
-
-        $emailTo = "rozetadavcevska@gmail.com";
-        $headers = "From: " .$firstName .$lastName. 
-        "\r\n Email: ".$emailFrom. 
-        "\r\n Message: " .$message. "\r\n";
-
-        mail($emailTo, $firstName, $message, $headers);
-    } else{
-        echo json_encode(["sent" => false, "message" => "Something went wrong"]);
+        // validating the email
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors[] = 'Invalid email';
+        }
     }
+    if (empty($_POST['message'])) {
+        $errors[] = 'Message is empty';
+    } else {
+        $message = $_POST['message'];
+    }
+    if (empty($errors)) {
+        $date = date('j, F Y h:i A');
 
-   
+        $emailBody = "
+            Email from: $email 
+            Date: $date
+            Meassage: $message
+            First name: $fname
+            Last name: $lname
+        ";
 
+        $headers =     'From: Contact Form <contact@mydomain.com>' . "\r\n" .
+            "Reply-To: $email" . "\r\n" .
+            "MIME-Version: 1.0\r\n" .
+            "Content-type: text/plain; charset=utf-8";
+
+        $to = 'rozetadavcevska@gmail.com';
+        $subject = 'Contacting you';
+
+        if (mail($to, $subject, $emailBody, $headers)) {
+            $sent = true;
+        }
+    }
+}
 ?>
+
+<?php if (!empty($errors)) : ?>
+
+{
+"status": "fail",
+"error": <?php echo json_encode($errors) ?>
+}
+<?php endif; ?>
+
+
+<?php if (isset($sent) && $sent === true) : ?>
+
+{
+"status": "success",
+"message": "Your data was successfully submitted"
+}
+
+<?php endif; ?>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,136 +1,153 @@
 import React from 'react';
+import axios from 'axios';
 //import $ from 'jquery';
 import {Container, Form, Row, Col, Button} from "react-bootstrap";
 import {Footer} from '../components/components';
 
 //
-function Contact(){
+class Contact extends React.Component{
 
-    constructor(){
+    constructor(props){
         super(props);
         this.state = {
           fname: '',
           lname: '',
           email: '',
-          message: '',
-          mailSent: false,
-          error: null
+          message: ''
         }
       }
 
-      handleFormSubmit = e => {
+    handleSubmit(e){
         e.preventDefault();
         axios({
-          method: 'post',
-          url: `${API_PATH}`,
-          headers: { 'content-type': 'application/json' },
+          method: "POST",
+          url: "http://localhost:3000/formValidation.php",
           data: this.state
+        }).then((response) => {
+          if (response.data.status === 'success') {
+            alert("Message Sent.");
+            this.resetForm()
+          } else if (response.data.status === 'fail') {
+            alert("Message failed to send.")
+          }
         })
-          .then(result => {
-            this.setState({
-              mailSent: result.data.sent
-            })
-          })
-          .catch(error => this.setState({ error: error.message }));
-      };
+    }
+    
+    resetForm() {
+        this.setState({ fname: '', lname: '', email: '', message: '' })
+      }
+
+    onFNameChange(event) {
+        this.setState({ fname: event.target.value })
+    }
+
+    onLNameChange(event) {
+        this.setState({ lname: event.target.value })
+    }
+
+    onEmailChange(event) {
+            this.setState({ email: event.target.value })
+        }
+
+    onMessageChange(event) {
+            this.setState({ message: event.target.value })
+        }
+    
+    render(){
+        return(
+            <>
+            <section className="contact-section">
+                <Container className="contact-form bg-light mt-5 mb-5 p-5">
+                    <Container fluid>
+                        <h1>Contact Us</h1>
+                        <h3 className="mb-5">Please fill in your details below and submit your enquiry:</h3>
+                    </Container>
 
 
-    return(
-        <>
-        <section className="contact-section">
-            <Container className="contact-form bg-light mt-5 mb-5 p-5">
-                <Container fluid>
-                    <h1>Contact Us</h1>
-                    <h3 className="mb-5">Please fill in your details below and submit your enquiry:</h3>
+                    <Form 
+                        className="form-container" 
+                        action="./php/formValidation.php"
+                        method="post"
+                        name="form"
+                        onSubmit={this.handleSubmit.bind(this)}
+                        noValidate
+                    >
+                        <Row className="mb-3">
+                            <Form.Group as={Col} md="6" controlId="validationCustom01">
+                            <Form.Label>First name</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="First name"
+                                name="fname"
+                                value={this.state.fname}
+                                onChange={this.onFNameChange.bind(this)}
+                            />
+                            </Form.Group>
+                            <Form.Group className="last-name" as={Col} md="6" controlId="validationCustom02">
+                            <Form.Label>Last name</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="Last name"
+                                name="lname"
+                                value={this.state.lname}
+                                onChange={this.onLNameChange.bind(this)}
+                            />
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                        <Form.Group className="mb-3" controlId="formGroupEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control 
+                                required 
+                                type="email" 
+                                placeholder="Email" 
+                                name="email"
+                                value={this.state.email}
+                                onChange={this.onEmailChange.bind(this)}
+                            />
+                        </Form.Group>
+                        {/* <Form.Group className="mb-3" controlId="formGridAddress">
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control required placeholder="Street Address" name="address"/>
+                        </Form.Group> */}
+                        </Row>
+                        <Form.Group  className="mb-3" controlId="exampleForm.ControlTextarea">
+                            <Form.Label>How can we help you?</Form.Label>
+                            <Form.Control 
+                                required 
+                                as="textarea" 
+                                name="message" 
+                                rows={3} 
+                                value={this.state.message}
+                                onChange={this.onMessageChange.bind(this)}
+                            />
+                        </Form.Group>
+                        {/* <Form.Group className="mb-3">
+                            <Form.Check
+                                required
+                                label="Agree to terms and conditions"
+                                feedback="You must agree before submitting."
+                                feedbackType="invalid"
+                            />
+                        </Form.Group> */}
+                        <Button 
+                            className="bg-dark border-dark" 
+                            type="submit" 
+                            name="submit"
+                            onSubmit={this.handleSubmit.bind(this)}
+                            value="Submit"> Submit</Button>    
+                        </Form>
                 </Container>
+            </section>
+            
 
+            <Footer />
+            </>
+        )
+    }
 
-                <Form 
-                    className="form-container" 
-                    action="./php/formValidation.php"
-                    method="post"
-                    name="form"
-                    noValidate
-                >
-                    <Row className="mb-3">
-                        <Form.Group as={Col} md="6" controlId="validationCustom01">
-                        <Form.Label>First name</Form.Label>
-                        <Form.Control
-                            required
-                            type="text"
-                            placeholder="First name"
-                            name="fname"
-                            value={this.state.fname}
-                            onChange={e => this.setState({ fname: e.target.value })}
-                        />
-                        </Form.Group>
-                        <Form.Group className="last-name" as={Col} md="6" controlId="validationCustom02">
-                        <Form.Label>Last name</Form.Label>
-                        <Form.Control
-                            required
-                            type="text"
-                            placeholder="Last name"
-                            name="lname"
-                            value={this.state.lname}
-                            onChange={e => this.setState({ lname: e.target.value })}
-                        />
-                        </Form.Group>
-                    </Row>
-                    <Row>
-                    <Form.Group className="mb-3" controlId="formGroupEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control 
-                            required 
-                            type="email" 
-                            placeholder="Email" 
-                            name="email"
-                            value={this.state.email}
-                            onChange={e => this.setState({ email: e.target.value })}
-                        />
-                    </Form.Group>
-                    {/* <Form.Group className="mb-3" controlId="formGridAddress">
-                        <Form.Label>Address</Form.Label>
-                        <Form.Control required placeholder="Street Address" name="address"/>
-                    </Form.Group> */}
-                    </Row>
-                    <Form.Group  className="mb-3" controlId="exampleForm.ControlTextarea">
-                        <Form.Label>How can we help you?</Form.Label>
-                        <Form.Control 
-                            required 
-                            as="textarea" 
-                            name="message" 
-                            rows={3} 
-                            onChange={e => this.setState({ message: e.target.value })}
-                            value={this.state.message}
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Check
-                            required
-                            label="Agree to terms and conditions"
-                            feedback="You must agree before submitting."
-                            feedbackType="invalid"
-                        />
-                    </Form.Group>
-                    <Button 
-                        className="bg-dark border-dark" 
-                        type="submit" 
-                        name="submit"
-                        onClick={e => this.handleFormSubmit(e)} 
-                        value="Submit"> Submit</Button>
-                    <div>
-                        {this.state.mailSent &&
-                            <div>Thank you for contcting us.</div>
-                        }        
-                    </div>        
-                    </Form>
-            </Container>
-        </section>
-        
-
-        <Footer />
-        </>
-    )
 }
 
 export default Contact;
